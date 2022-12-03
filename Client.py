4d322062c7d77f5b5ea8b2bdb00c39a34a7ffbff
Yumi Go <yumigoyumi@gmail.com>
2022-12-03 12:01:16 +0000

    module id loop solved

--- Client.py
+++ Client.py
@@ -1,3 +1,4 @@
+import pickle
 import socket
 
 SERVER = "127.0.0.1"
@@ -8,48 +9,64 @@ sock.connect((SERVER, PORT))
 
 response = ""
 data_to_send_moduleID = ""
-learning_outcome = "1. Evaluate and apply design patterns in the design and development of a distributed system.\n" \
-                   "2. Assess and apply different architectural patterns in a distributed system.\n" \
-                   "3. Critically access and apply threading in a distributed application.\n" \
-                   "4. Debug a distributed client/server application, identifying object properties and variables at run-time.\n" \
-                   "5. Create a distributed object application using RMI, allowing client/server to communicate securely via interfaces and objects.\n"
+# learning_outcome = "1. Evaluate and apply design patterns in the design and development of a distributed system.\n" \
+#                    "2. Assess and apply different architectural patterns in a distributed system.\n" \
+#                    "3. Critically access and apply threading in a distributed application.\n" \
+#                    "4. Debug a distributed client/server application, identifying object properties and variables at run-time.\n" \
+#                    "5. Create a distributed object application using RMI, allowing client/server to communicate securely via interfaces and objects.\n"
+
+learning_outcome = []
 
 while response != 'X' or response != 'x':
 
-    # 1. send module ID
-    data_to_send_moduleID = input("What is the module id? ")
-    sock.sendall(bytes(data_to_send_moduleID, 'UTF-8'))
+    while True:
+        # 1. send module ID
+        data_to_send_moduleID = input("What is the module id? ")
+        sock.sendall(bytes(data_to_send_moduleID, 'UTF-8'))
+        # 4. receive true or false for existence of module ID in data dictionary from the server
+        data = sock.recv(1024)
+        # print(data) # for check
+        # print(repr(data))
+        bool_data = bool(int(data))
+        print(bool_data)  # for check
+        if bool_data:
+            response = input("(L)earning Outcomes, (C)ourses, (A)ssessments or e(X)it? ")
 
-    # learning outcomes
-    # 2. receive true or false for existence of module ID in data dictionary
-    data = sock.recv(1024) # receive True or False
-    # print(data) # for check
-    # print(repr(data))
+            if response == 'L' or response == 'l':
 
-    # 4. if client get True value from server / or not
-    if data:
-        response = input("(L)earning Outcomes, (C)ourses, (A)ssessments or e(X)it? ")
+                # 5. send the request for Learning Outcomes to the server
+                sock.sendall(bytes("request LO", 'UTF-8'))
 
-        if response == 'L' or response == 'l':
-            sock.sendall(bytes(data_to_send_moduleID, 'UTF-8'))
-            print(learning_outcome)
-            lo_answer = ''
-            while lo_answer != 'R' or lo_answer != 'r':
-                lo_answer = input("(A)dd, (E)dit, (D)elete or (R)eturn? ")
-                if lo_answer == 'A' or lo_answer == 'a':
-                    print("you choose Add option in Learning Outcomes menu")
-                    add_lo = input("Enter new LO description: ")
-                    sock.sendall(bytes(add_lo, 'UTF-8'))
-                    data = sock.recv(1024)
-                    print("Updated LO List: \n")
-                elif lo_answer == 'E' or lo_answer == 'e':
-                    print("you choose Edit option in Learning Outcomes menu")
-                elif lo_answer == 'D' or lo_answer == 'd':
-                    print("you choose Delete option in Learning Outcomes menu")
-                else:
-                    print("Enter the correct answer")
-    else:
-        print("error")
+                # 8. receive the Learning Outcomes from the server
+                data = sock.recv(1024)
+                lo_list = pickle.loads(data)
+                # print(type(lo_list)) # for check
+                # print(lo_list) # for check
+                for i in range(len(lo_list)):
+                    print(f'{i+1}. {lo_list[i]}')
+
+                lo_answer = ''
+                while lo_answer != 'R' or lo_answer != 'r':
+                    lo_answer = input("(A)dd, (E)dit, (D)elete or (R)eturn? ")
+                    if lo_answer == 'A' or lo_answer == 'a':
+                        print("you choose Add option in Learning Outcomes menu")
+                        add_lo = input("Enter new LO description: ")
+
+                        # 9. send the Learning Outcome to be added
+                        sock.sendall(bytes(add_lo, 'UTF-8'))
+
+
+                        data = sock.recv(1024)
+                        print("Updated LO List: \n", data)
+                    elif lo_answer == 'E' or lo_answer == 'e':
+                        print("you choose Edit option in Learning Outcomes menu")
+                    elif lo_answer == 'D' or lo_answer == 'd':
+                        print("you choose Delete option in Learning Outcomes menu")
+                    else:
+                        print("Enter the correct answer")
+            break
+        else:
+            print("Enter the correct Module ID!")
 
     data = sock.recv(1024)
     print('Received', repr(data))
--- Server.py
+++ Server.py
@@ -1,11 +1,48 @@
+import pickle
 import socket
 import threading
 
 my_dictionary = {
-    "SOFT8023": [["lo1", "lo2", "lo3"], ["prog1", "prog2"], ["assess1", "assess2"]],
-    "SOFT8009": [["lo1", "lo2", "lo3"], ["prog1", "prog2"], ["assess1", "assess2"]]
+    "SOFT8023": [
+        [
+            "Evaluate and apply design patterns in the design and development of a distributed system.",
+            "Assess and apply different architectural patterns in a distributed system.",
+            "Critically access and apply threading in a distributed application.",
+            "Debug a distributed client/server application, identifying object properties and variables at run-time.",
+            "Create a distributed object application using RMI, allowing client/server to communicate securely via interfaces and objects."
+        ],
+        [
+            "prog1",
+            "prog2"
+        ],
+        [
+            "assess1",
+            "assess2"
+        ]
+    ],
+    "SOFT8009":
+        [
+            [
+                "lo1",
+                "lo2",
+                "lo3"
+            ],
+            [
+                "prog1",
+                "prog2"
+            ],
+            [
+                "assess1",
+                "assess2"
+            ]
+        ]
 }
 
+# learning_outcome = "1. Evaluate and apply design patterns in the design and development of a distributed system.\n" \
+#                    "2. Assess and apply different architectural patterns in a distributed system.\n" \
+#                    "3. Critically access and apply threading in a distributed application.\n" \
+#                    "4. Debug a distributed client/server application, identifying object properties and variables at run-time.\n" \
+#                    "5. Create a distributed object application using RMI, allowing client/server to communicate securely via interfaces and objects.\n"
 
 
 
@@ -19,28 +56,53 @@ class ClientThread(threading.Thread):
 
     def run(self):
         print("Connection from : ", clientAddress)
+
         while True:
-            # 2. receive module ID from client
-            data = self.c_socket.recv(1024)
-            if not data:
-                break
-            module_id = data.decode()
-            print("from client", module_id)
-            if module_id in list(my_dictionary):
-                # print(list(my_dictionary))
-                # 3. send boolean value to client (for existence of module ID in dictionary)
-                self.c_socket.send(bytes('True', 'UTF-8'))
+            while True:
+                # 2. receive module ID from client
+                data = self.c_socket.recv(1024)
+                if not data:
+                    break
+                module_id = data.decode()
+                print("from client", module_id)
 
-            # Add learning outcome
+                if module_id in list(my_dictionary):
+                    # print(list(my_dictionary))
+                    # 3. send boolean value to client (for existence of module ID in dictionary)
+                    self.c_socket.send(bytes('1', 'UTF-8'))
+                    break
+                # if module_id is not in my_dictionary, loop until it matches
+                self.c_socket.send(bytes('0', 'UTF-8'))
+
+
+            # while True:
+            #     if module_id in list(my_dictionary):
+            #         # print(list(my_dictionary))
+            #         # 3. send boolean value to client (for existence of module ID in dictionary)
+            #         self.c_socket.send(bytes('True', 'UTF-8'))
+            #         break
+            #     else:
+            #         self.c_socket.send(bytes('Enter the correct Module ID!', 'UTF-8'))
+
+            # 6. receive the request for Learning Outcomes from the client
             data = self.c_socket.recv(1024)
             if not data:
                 break
-            add_lo = data.decode()
-            added_result = str(my_dictionary[module_id][0].append(add_lo))
+            # if data.decode() == "request LO":
+                # self.c_socket.send(bytes(str(my_dictionary[module_id][0]), 'UTF-8'))
+                # for line in my_dictionary[module_id][0]:
+                #     print(line) # for check
+                #     self.c_socket.send(bytes(line), 'UTF-8')
+
+            # 7. send the Learning Outcomes to the client
+            lo_to_send = pickle.dumps(my_dictionary[module_id][0])
+            self.c_socket.send(lo_to_send)
+
+            # 10. receive the Learning Outcome to be added
+            data = self.c_socket.recv(1024)
+            lo_to_add = data.decode()
+            added_result = str(my_dictionary[module_id][0].append(lo_to_add))
             self.c_socket.send(bytes(added_result, 'UTF-8'))
-
-
-
 
         print("Client at ", clientAddress, " disconnected...")
 
